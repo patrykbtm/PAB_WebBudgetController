982648b03bb845539db6e2c87897f1c87dbea9dc
Patryk Bocheński <pbochenski@insolutions.pl>
2017-11-10 23:51:11 +0100

    Dodanie plikow projektu.

---
+++ PAB_WebBudgetController/PAB_WebBudgetController.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PAB_WebBudgetController.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
---
+++ PAB_WebBudgetController/PAB_WebBudgetController/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PAB_WebBudgetController.MvcApplication" Language="C#" %>
